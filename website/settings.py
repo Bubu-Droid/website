5933df0fdbfe3fad16721543afdde60c2791e997
Bubu-Droid <fufudadw@gmail.com>
2025-07-12 18:57:44 +0530

    Set up HSTS

--- website/settings.py
+++ website/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["www.bubudroid.me", "127.0.0.1", ".localhost"]
 
-ADMINS = [("BubuDroid8207", "fufudadw@email.com")]
+ADMINS = [("BubuDroid", "fufudadw@email.com")]
 
 # TODO: un-comment this before pushing the code
 
@@ -42,6 +42,10 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = "DENY"
+
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 # Application definition
 
