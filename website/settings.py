06e3169dedb653058dea743376b0e8d3c3468888
Bubu-Droid <fufudadw@gmail.com>
2025-07-13 00:04:37 +0530

    Configure error emailing

--- website/settings.py
+++ website/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-from website.rc import SECRET_KEY
+try:
+    from website.rc import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
