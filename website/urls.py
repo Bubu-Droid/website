2ca30458d778339669f5bb4a3094ad1f57ed8d1e
Bubu-Droid <fufudadw@gmail.com>
2026-06-21 20:34:32 +0530

    Remove redundant import

--- website/urls.py
+++ website/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 
